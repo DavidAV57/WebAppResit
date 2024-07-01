040034413594e1de5ee1c009787ef9b4882ce007
David <2205080@chester.ac.uk>
2024-07-01 15:35:56 +0100

    Commiting now because first attempt at database wouldnt load :'(

---
+++ WebAppResit/Models/BookItems.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebAppResit.Models;
+
+public class BookItems
+{
+    public class Books
+    {
+        [Key]
+        [StringLength(13, MinimumLength = 10)] // ISBN-10 or ISBN-13
+        public string ISBN { get; set; }
+
+        [StringLength(30)]
+        public string ItemName { get; set; }
+
+        [StringLength(255)]
+        public string Item_desc { get; set; }
+        [StringLength(40)]
+        public string Author { get; set; }
+            
+        public Nullable<bool> Available { get; set; }
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "Money")]
+        public Nullable<decimal> Price { get; set; }
+        
+    }
+}
