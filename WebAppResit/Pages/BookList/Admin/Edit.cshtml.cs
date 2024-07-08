4003c6d8c1319e98af720c690919c397fa0fea6f
David <2205080@chester.ac.uk>
2024-07-08 09:59:21 +0100

    Make sure everything is pushed

--- WebAppResit/Pages/BookList/Admin/Edit.cshtml.cs
+++ WebAppResit/Pages/BookList/Admin/Edit.cshtml.cs
@@ -38,9 +38,7 @@ namespace WebAppResit.Pages.Menu
             BookItem = bookitem;
             return Page();
         }
-
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see https://aka.ms/RazorPagesCRUD.
+        
         public async Task<IActionResult> OnPostAsync()
         {
             if (!ModelState.IsValid)
